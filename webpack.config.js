e0dd151d88f0bfeb05d29da2c79e561f89d67409
Anderson <anderson@tresna.co>
2017-12-26 15:56:23 -0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path')
-
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js'
+    filename: './dist/app.bundle.js',
+    libraryTarget: 'commonjs2'
   },
   module: {
     loaders: [{
